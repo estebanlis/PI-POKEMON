b8fa886cf7bb76231af057ffe651af40ed1cd21b
Esteban Cortez <estebanpd@gmail.com>
2022-04-10 22:15:28 -0300

    models Type

--- api/index.js
+++ api/index.js
@@ -18,10 +18,23 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const { Type, conn } = require('./src/db.js');
+const axios = require('axios');
+
+
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: true }).then(async () => {
+  try {
+    let TypesFromApi = await axios.get('https://pokeapi.co/api/v2/type/');
+    let types = [];
+
+    TypesFromApi.data.results.map( type => types.push({name: type.name}));
+    await Type.bulkCreate(types);
+
+  } catch (error) {
+      console.log('Erro from api Types: ',error.message);
+  }
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
