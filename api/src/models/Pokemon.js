504ce82b1589dd45e0b2e3b0ce8e3b4e0131eb72
Cortez <esteban.cortez@albiero.net>
2022-04-12 16:59:38 -0300

    update

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -5,13 +5,52 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
     id: {
-      type: DataTypes.UUID,
-      primaryKey: true
-
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      get() {
+        return this.getDataValue('id')+'B';
+      }
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    hp: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+
+    },
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: "https://user-images.githubusercontent.com/6562688/65653705-3074a880-dfe4-11e9-8cfe-d2c62112587e.jpg",
+
+    },
+
   });
 };
--- api/src/routes/pokemon.js
+++ api/src/routes/pokemon.js
@@ -1,15 +1,18 @@
 const {Router} = require('express');
 const axios = require('axios');
 
+const {Pokemon, Type} = require('../db');
 const router = Router();
 
 const urlApi = 'https://pokeapi.co/api/v2/pokemon';
 
 router.get('/', async (req, res) => {
 
+   
+
     try {
 
-        let api_1 = await axios.get(urlApi + '?limit=10&offset=0'); 
+        let api_1 = await axios.get(urlApi + '?limit=40&offset=0'); 
     // data.results -->       {
     //                          "name": "bulbasaur",
     //                          "url": "https://pokeapi.co/api/v2/pokemon/1/"
@@ -23,6 +26,7 @@ router.get('/', async (req, res) => {
     let pokemons = api_3.map( pok => {
         let obj = {};
         obj = {
+            id: pok.data.id,
             name: pok.data.name,
             image: pok.data.sprites.other.dream_world.front_default,
             type: pok.data.types.length > 0 ? pok.data.types.map( t => t.type.name) : []
@@ -37,6 +41,90 @@ router.get('/', async (req, res) => {
     }
 
     
+});
+
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        let pokemon = await axios.get(urlApi + '/' + id); // trae pokemon desde la api por el id
+        
+        let obj = {
+            id: pokemon.data.id,
+            name: pokemon.data.name,
+            hp: pokemon.data.stats[0].base_stat,
+            attack: pokemon.data.stats[1].base_stat,
+            defense: pokemon.data.stats[2].base_stat,
+            speed: pokemon.data.stats[5].base_stat,
+            height: pokemon.data.height,
+            weight: pokemon.data.weight,
+            image: pokemon.data.sprites.other.dream_world.front_default,
+            type: pokemon.data.types.length > 0 ? pokemon.data.types.map( t => t.type.name) : []
+        }
+
+        console.log(obj);
+
+        return res.json(obj);
+        
+    } catch (error) {
+        res.status(404).json({msg: 'not found'}); 
+    }
+});
+
+router.post('/', async (req, res) => {
+    const {name,hp,attack,defense,speed,height,weight,image,types} = req.body;
+
+    try {
+        const newPokemon = await Pokemon.create(
+            {
+                name,
+                hp,
+                attack,
+                defense,
+                speed,
+                height,
+                weight,
+                image
+                
+            }
+        );
+
+        newPokemon.setTypes(types);
+
+        /* let pok = await Pokemon.findByPk(1,
+            {
+                include: [{
+                    model: Type,
+                    through: {attributes: []}
+                }]
+            });
+         */
+            let pok = await Pokemon.findByPk(1);
+            let i = await pok.getTypes();
+            
+         
+            let obj = {
+                id: pok.id,
+                name: pok.name,
+                hp: pok.hp,
+                attack: pok.attack,
+                defense: pok.defense,
+                speed: pok.speed,
+                height: pok.height,
+                weight: pok.weight,
+                image: pok.image,
+                type: i.length > 0 ? i.map( t => t.name) : []
+            } 
+            
+
+        //console.log(pok.toJSON());
+      //let i = await pok.getTypes();
+
+        res.json(obj);
+
+    } catch (error) {
+        res.status(404).json(error.message); 
+    }
+
 });
 
 module.exports = router;
